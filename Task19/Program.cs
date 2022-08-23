ab8cf6dbcce3986bd7e5296340e5804ddbb81b10
OlgaGord <gordeewa06@gmail.com>
2022-08-23 18:25:21 +0500

    Задача 19 решена

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,3 +4,20 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+// string val = "12821";
+
+
+string Compare(string val, int i, int j)
+{
+    if (val[i]==val[j])
+    {
+        if (val[i+1]==val[j-1]) Console.WriteLine("да");
+        else Console.WriteLine("нет");
+    }
+    else Console.WriteLine("нет");
+return val;
+}
+
+Compare("12821", 0, 4);
+
